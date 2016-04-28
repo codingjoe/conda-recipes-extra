0ae7794bef491f7a622f95387b270a1642c3c8b6
Johannes Hoppe <info@johanneshoppe.com>
2016-04-28 10:56:27 +0200

    wip

--- graph-tool/build.sh
+++ graph-tool/build.sh
@@ -16,10 +16,9 @@ export PKG_INSTALLDIR="$PREFIX"
 export CC="gcc-5"
 export CXX="g++-5"
 ./autogen.sh
-./configure --enable-openmp --enable-static --prefix="$PREFIX" --with-boost="$PREFIX" --with-boost-libdir="$PREFIX/lib" || { cat config.log ; exit 1 ; }  #-I$PREFIX/include/cairo -I$PREFIX/include/cairomm-1.0 -I$PREFIX/include/pycairo -I$PREFIX/include/sigc++-2.0 -I$PREFIX/include/freetype2 -I$PREFIX/lib/cairomm-1.0/include -I$PREFIX/lib/sigc++-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"   
+./configure --enable-openmp --prefix="$PREFIX" --with-boost="$PREFIX" --with-boost-libdir="$PREFIX/lib" || { cat config.log ; exit 1 ; }  #-I$PREFIX/include/cairo -I$PREFIX/include/cairomm-1.0 -I$PREFIX/include/pycairo -I$PREFIX/include/sigc++-2.0 -I$PREFIX/include/freetype2 -I$PREFIX/lib/cairomm-1.0/include -I$PREFIX/lib/sigc++-2.0/include -I/usr/include -I/usr/include/X11 -I/usr/include/X11/extensions"   
 make
 make check
 make install
 
 unlink $PREFIX/lib64
-
